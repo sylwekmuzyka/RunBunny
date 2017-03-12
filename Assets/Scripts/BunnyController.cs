8908db4dab6c807820488dfb8e94b6a091b51876
sylwekmuzyka <SylwekMuzyka@gmail.com>
2017-03-12 15:21:15 +0100

    Stworzenie projektu
    - dodanie postaci królika, skakanie, kolizje

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour
+{
+	private Rigidbody2D rigidBody;
+	public float bunnyJumpForce = 750f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetButtonUp("Jump") || Input.GetMouseButtonDown(0))
+		{
+			rigidBody.AddForce(transform.up * bunnyJumpForce);
+		}
+	}
+}
