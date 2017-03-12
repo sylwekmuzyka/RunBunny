f80c63c8a6919d83d9b909979e28353ce62f0aa2
sylwekmuzyka <SylwekMuzyka@gmail.com>
2017-03-12 16:11:50 +0100

    Dodanie animacji królika

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class BunnyController : MonoBehaviour
 {
 	private Rigidbody2D rigidBody;
+	private Animator animator;
 	public float bunnyJumpForce = 750f;
 
 	// Use this for initialization
 	void Start ()
 	{
 		rigidBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		if(Input.GetButtonUp("Jump") || Input.GetMouseButtonDown(0))
-		{
 			rigidBody.AddForce(transform.up * bunnyJumpForce);
-		}
+
+		animator.SetFloat("vVelocity", Mathf.Abs(rigidBody.velocity.y));
 	}
 }
