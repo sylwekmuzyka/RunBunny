fbada70f0c8b9bdf2c0600e9e637e113ab9345cc
sylwekmuzyka <SylwekMuzyka@gmail.com>
2017-03-13 23:28:08 +0100

    Dodanie kaktusów, kolizji z nimi, punktów

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -1,26 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BunnyController : MonoBehaviour
 {
 	private Rigidbody2D rigidBody;
 	private Animator animator;
 	public float bunnyJumpForce = 750f;
+	private float bunnyHurtTime = -1;
+	private Collider2D collider;
+	public Text scoreText;
+	private float startTime;
 
 	// Use this for initialization
 	void Start ()
 	{
 		rigidBody = GetComponent<Rigidbody2D>();
 		animator = GetComponent<Animator>();
+		collider = GetComponent<Collider2D>();
+
+		startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if(Input.GetButtonUp("Jump") || Input.GetMouseButtonDown(0))
-			rigidBody.AddForce(transform.up * bunnyJumpForce);
+		if(bunnyHurtTime == -1)
+		{
+			if(Input.GetButtonUp("Jump") || Input.GetMouseButtonDown(0))
+				rigidBody.AddForce(transform.up * bunnyJumpForce);
 
-		animator.SetFloat("vVelocity", Mathf.Abs(rigidBody.velocity.y));
+			animator.SetFloat("vVelocity", rigidBody.velocity.y);
+			scoreText.text = (Time.time - startTime).ToString("0.0");
+		}
+		else
+		{
+			if(Time.time > bunnyHurtTime + 2)
+				Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+		{
+			foreach(PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())
+				spawner.enabled = false;
+
+			foreach(MoveLeft moveLefter in FindObjectsOfType<MoveLeft>())
+				moveLefter.enabled = false;
+
+			bunnyHurtTime = Time.time;
+			animator.SetBool("bunnyHurt", true);
+			rigidBody.velocity = Vector2.zero;
+			rigidBody.AddForce(transform.up * bunnyJumpForce);
+			collider.enabled = false;
+		}
 	}
 }
